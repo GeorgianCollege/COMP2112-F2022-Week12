2bcced98b5bec2be5f7144fb4010be2bfa572ef4
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-11-28 10:06:33 -0500

    added simple html file

--- server.js
+++ server.js
@@ -4,10 +4,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const http_1 = __importDefault(require("http"));
+const fs_1 = __importDefault(require("fs"));
 let port = 3000;
 let server = http_1.default.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello!");
+    fs_1.default.readFile(__dirname + req.url, (err, data) => {
+        if (err) {
+            res.writeHead(404);
+            res.end(JSON.stringify(err));
+            return;
+        }
+        res.writeHead(200);
+        res.end(data);
+    });
 });
 server.listen(port, 'localhost', () => {
     console.log(`Listening on Port: ${port}`);
--- server.ts
+++ server.ts
@@ -1,11 +1,20 @@
 import http from 'http';
+import fs from 'fs';
 
 let port: number = 3000;
 
 // creating an instance of the server object
 let server = http.createServer((req, res)=>{
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end("Hello!");
+    fs.readFile(__dirname + req.url, (err, data)=>{
+        if(err)
+        {
+            res.writeHead(404); // file not found
+            res.end(JSON.stringify(err));
+            return;
+        }
+        res.writeHead(200);
+        res.end(data);
+    });
 });
 
 server.listen(port, 'localhost', ()=>{
