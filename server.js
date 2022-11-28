559b61f73883a018f66c74154dac8557347d44dc
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-11-28 10:41:42 -0500

    sanitized path

--- server.js
+++ server.js
@@ -5,16 +5,29 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const http_1 = __importDefault(require("http"));
 const fs_1 = __importDefault(require("fs"));
+const mime_types_1 = __importDefault(require("mime-types"));
+const path_1 = __importDefault(require("path"));
+let lookup = mime_types_1.default.lookup;
 let port = 3000;
 let host = "localhost";
 let server = http_1.default.createServer((req, res) => {
-    fs_1.default.readFile(__dirname + req.url, (err, data) => {
+    let rawURL = req.url;
+    let parsedURL = new URL(rawURL, `http://${host}:${port}`);
+    let parsedPath = parsedURL.pathname.replace(/^\/+|\/+$/g, "");
+    if (parsedPath == "") {
+        parsedPath = "index.html";
+    }
+    let file = path_1.default.join(__dirname, "./Client") + "\\" + parsedPath;
+    console.log(file);
+    fs_1.default.readFile(file, (err, data) => {
         if (err) {
             res.writeHead(404);
             res.end(JSON.stringify(err));
             return;
         }
-        res.writeHead(200);
+        res.setHeader("X-Content-Type-Options", "nosniff");
+        let mimeType = lookup(parsedPath);
+        res.writeHead(200, "", { "Content-Type": mimeType });
         res.end(data);
     });
 });
--- server.ts
+++ server.ts
@@ -1,20 +1,39 @@
 import http from 'http';
 import fs from 'fs';
 import url from 'url';
+import mime from 'mime-types';
+import path from 'path';
 
+let lookup = mime.lookup; // alias for mime.lookup
 let port: number = 3000;
 let host: string = "localhost";
 
+
 // creating an instance of the server object
 let server = http.createServer((req, res)=>{
-    fs.readFile(__dirname + req.url, (err, data)=>{
+
+    let rawURL = req.url as string;
+    let parsedURL = new URL(rawURL, `http://${host}:${port}`);
+    
+    let parsedPath = parsedURL.pathname.replace(/^\/+|\/+$/g, "");
+    if(parsedPath == "")
+    {
+        parsedPath = "index.html";
+    }
+
+    let file = path.join(__dirname, "./Client") + "\\" + parsedPath;
+    console.log(file);
+
+    fs.readFile(file, (err, data)=>{
         if(err)
         {
             res.writeHead(404); // file not found
             res.end(JSON.stringify(err));
             return;
         }
-        res.writeHead(200);
+        res.setHeader("X-Content-Type-Options", "nosniff"); // path security
+        let mimeType = lookup(parsedPath) as string;
+        res.writeHead(200, "", { "Content-Type": mimeType });
         res.end(data);
     });
 });
